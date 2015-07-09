885e3fabdd6e4411a84417005ab015fd56ade3ba
kurtcunningham <cunningham.kurt@gmail.com>
2015-07-08 19:34:28 -0500

    large refactor, removed assets pipeline because it was not supported

--- _assets/javascript/app.js
+++
@@ -1,3 +0,0 @@
-//= require vendor/jquery
-//= require vendor/map-options
-//= require_tree .
--- _plugins/ext.rb
+++
@@ -1,7 +0,0 @@
-require 'sass'
-require 'uglifier'
-require 'coffee-script'
-
-require 'jekyll-assets'
-require 'jekyll-assets/bourbon'
-require 'jekyll-assets/neat'
