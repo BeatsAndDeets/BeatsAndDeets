2af999ff4a8be28d7ff59c2e68821125affecf82
kurtcunningham <cunningham.kurt@gmail.com>
2015-09-03 18:00:56 -0500

    removed script

--- js/components/member-card.js
+++ js/components/member-card.js
@@ -1,30 +1,30 @@
-var cardHeight;
-// Sets the height of the back of the card to match the image in front
-function setBackHeight() {
-  cardHeight = $('.member .member--front img').height();
-  $('.member .member--back').height(cardHeight);
-}
-$(document).ready(function() {
-  $('.member').addClass('not-flipped');
-  if (Modernizr.touch){
-    $('.member.not-flipped').on('click', function() {
-      $('.member').addClass('not-flipped');
-      $(this).removeClass('not-flipped');
-    });
-    $('.cancel-card').click( function(ev) {
-      ev.stopPropagation();
-      $('.member').addClass('not-flipped');
-    });
-  } else {
-    $('.member').hover(function() {
-      $(this).toggleClass('not-flipped');
-    });
-  }
-});
-$(window).load(function() {
-  // On window change, recalculate the size of the box
-  window.onresize = function(){
-    setBackHeight();
-  }
-  setBackHeight();
-});
+// var cardHeight;
+// // Sets the height of the back of the card to match the image in front
+// function setBackHeight() {
+//   cardHeight = $('.member .member--front img').height();
+//   $('.member .member--back').height(cardHeight);
+// }
+// $(document).ready(function() {
+//   $('.member').addClass('not-flipped');
+//   if (Modernizr.touch){
+//     $('.member.not-flipped').on('click', function() {
+//       $('.member').addClass('not-flipped');
+//       $(this).removeClass('not-flipped');
+//     });
+//     $('.cancel-card').click( function(ev) {
+//       ev.stopPropagation();
+//       $('.member').addClass('not-flipped');
+//     });
+//   } else {
+//     $('.member').hover(function() {
+//       $(this).toggleClass('not-flipped');
+//     });
+//   }
+// });
+// $(window).load(function() {
+//   // On window change, recalculate the size of the box
+//   window.onresize = function(){
+//     setBackHeight();
+//   }
+//   setBackHeight();
+// });
